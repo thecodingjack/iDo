dbf31a872334fa4cb3d3d4a06111aa1dd96cc4bd
Jack Lim <jacklim.tkl@gmail.com>
2018-07-24 10:49:46 -0400

    got google profile data with passport JS

--- server/index.js
+++ server/index.js
@@ -1,81 +1,16 @@
-let express = require('express')
-let session = require('express-session')
-let morgan = require('morgan');
-let parser = require('body-parser');
-let cors = require('cors');
-const readline = require('readline');
-const {google} = require('googleapis');
-const {GOOGLE_CLIENTID,GOOGLE_CLIENTSECRET,GOOGLE_redirect_uris} = require('./config')
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
-
-let app = express()
-
-let port = process.env.PORT || 3000;
-
-app.set('port',port);
-app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(morgan('dev'));
-app.use(session({
-  secret: 'super-secret-12345',
-  resave: true,
-  saveUninitialized: true
-}));
-
-app.use(express.static(__dirname + '/../client/dist'));
-
-const oAuth2Client = new google.auth.OAuth2(GOOGLE_CLIENTID,GOOGLE_CLIENTSECRET, 'http://localhost:3000/auth/google/callback');
-app.get('/auth/google',(req,res)=>{
-  if(!req.session.token) return getNewToken(oAuth2Client,res)
-  oAuth2Client.setCredentials(req.session.token)
-  res.send(req.session.token)
-})
-
-app.use('/auth/google/callback',(req,res)=>{
-  oAuth2Client.getToken(req.query.code, (err, mToken) => {
-    if (err) return res.send(err);
-    oAuth2Client.setCredentials(mToken);
-    req.session.token = mToken;
-    res.redirect('/');
-  });
-})
-
-function getNewToken(oAuth2Client,res){
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  res.redirect(authUrl);
-}
-
-app.get('/*',(req,res)=>{
-  res.redirect('/')
-})
-
-app.listen(port,()=> console.log('Listening on : ' + port))
-
-module.exports.app = app;
-
 // let express = require('express')
 // let session = require('express-session')
 // let morgan = require('morgan');
 // let parser = require('body-parser');
 // let cors = require('cors');
-// const passport = require('passport')
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const {GOOGLE_CLIENTID,GOOGLE_CLIENTSECRET} = require('./config.js')
+// const readline = require('readline');
+// const {google} = require('googleapis');
+// const {GOOGLE_CLIENTID,GOOGLE_CLIENTSECRET,GOOGLE_redirect_uris} = require('./config')
+// const SCOPES = ['https://www.googleapis.com/auth/plus.login'];
+
 // let app = express()
+
 // let port = process.env.PORT || 3000;
-
-// passport.use(new GoogleStrategy({
-//   clientID: GOOGLE_CLIENTID,
-//   clientSecret: GOOGLE_CLIENTSECRET,
-//   callbackURL: "http://localhost:3000/auth/google/callback"
-//   },(accessToken,refreshToken,profile,done)=>{
-//   console.log(accessToken)
-// }))
-
 
 // app.set('port',port);
 // app.use(parser.json());
@@ -90,15 +25,36 @@ module.exports.app = app;
 
 // app.use(express.static(__dirname + '/../client/dist'));
 
-// app.get(
-//   '/auth/google',
-//   passport.authenticate('google',{ scope: ['https://www.googleapis.com/auth/plus.login'] }),
-//   (req,res)=> res.send(""))
+// const oAuth2Client = new google.auth.OAuth2(GOOGLE_CLIENTID,GOOGLE_CLIENTSECRET, 'http://localhost:3000/auth/google/callback');
+// app.get('/auth/google',(req,res)=>{
+//   console.log(req.session.token)
+//   if(!req.session.token) return getNewToken(oAuth2Client,res)
+//   oAuth2Client.setCredentials(req.session.token)
+//   res.send(req.session.token)
+// })
 
-// app.get(
-//   '/auth/google/callback',
-//   passport.authenticate('google'),
-//   (req,res)=> res.send(""))
+// app.get('/api/logout',(req,res)=>{
+//   delete req.session.token;
+//   res.redirect('/')
+// })
+
+// app.use('/auth/google/callback',(req,res)=>{
+//   oAuth2Client.getToken(req.query.code, (err, mToken) => {
+//     if (err) return res.send(err);
+//     oAuth2Client.setCredentials(mToken);
+//     req.session.token = mToken;
+//     res.redirect('/');
+//   });
+// })
+
+// function getNewToken(oAuth2Client,res){
+//   const authUrl = oAuth2Client.generateAuthUrl({
+//     access_type: 'offline',
+//     scope: SCOPES,
+//   });
+//   console.log(authUrl)
+//   res.redirect(authUrl);
+// }
 
 // app.get('/*',(req,res)=>{
 //   res.redirect('/')
@@ -107,3 +63,61 @@ module.exports.app = app;
 // app.listen(port,()=> console.log('Listening on : ' + port))
 
 // module.exports.app = app;
+
+let express = require('express')
+let session = require('express-session')
+let morgan = require('morgan');
+let parser = require('body-parser');
+let cors = require('cors');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const {GOOGLE_CLIENTID,GOOGLE_CLIENTSECRET} = require('./config.js')
+let app = express()
+let port = process.env.PORT || 3000;
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENTID,
+  clientSecret: GOOGLE_CLIENTSECRET,
+  callbackURL: "http://localhost:3000/auth/google/callback"
+  },(accessToken,refreshToken,profile,done)=>{
+  console.log(accessToken);
+  console.log(refreshToken);
+  console.log(profile);
+}))
+
+
+app.set('port',port);
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('dev'));
+app.use(session({
+  secret: 'super-secret-12345',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google',{ scope: ['profile','email'] }),
+  (req,res)=> res.send(""))
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google'),
+  (req,res)=> res.redirect("/"))
+
+app.get('/api/logout', (req,res)=>{
+  req.logout();
+  res.send(req.user)
+})
+
+app.get('/*',(req,res)=>{
+  res.redirect('/')
+})
+
+app.listen(port,()=> console.log('Listening on : ' + port))
+
+module.exports.app = app;
