b304d6c07dd4a9b5c0b3ae7bec67db5f955ba626
Jack Lim <jacklim.tkl@gmail.com>
2018-07-29 10:58:10 -0400

    preparing for deployment 12

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(passport.session())
 app.use('/auth',authRouter)
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
   })
